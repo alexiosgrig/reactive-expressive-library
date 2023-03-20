4cbb484c909490210a9956e2344d553534645b78
agrigo <agrigo@netcompany-intrasoft.com>
2023-03-21 00:32:46 +0200

    route separation

--- backend/app.js
+++ backend/app.js
@@ -3,25 +3,40 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/',(res,req)=>{
-    console.log('response')
-    req.json({papades: 'papades agori mou'})
-})
-app.get('/papades',()=>{
-    console.log('papades')
-})
-app.get('/done',()=>{
-    console.log('done')
-})
-app.get('/zoo',()=>{
-    console.log('zoo')
-})
-app.get('/vet',()=>{
-    console.log('vet')
-})
-app.get('/music',()=>{
-    console.log('music')
-})
+const papadesId = (req,res)=>{
+    console.log(req.params)
+    res.status(200).json({data:req.params.id})
+}
+
+const papadesRouter = express.Router();
+app.use('/api/v1/papades',papadesId)
+
+
+
+
+
+papadesRouter.get('/',papadesId)
+papadesRouter.get('/:id',papadesId)
+
+
+
+// app.get('/',(res,req)=>{
+//     console.log('response')
+//     req.json({papades: 'papades agori mou'})
+// })
+// app.get('/papades/:id',papadesId)
+// app.get('/done',()=>{   
+//     console.log('done')
+// })
+// app.get('/zoo',()=>{
+//     console.log('zoo')
+// })
+// app.get('/vet',()=>{
+//     console.log('vet')
+// })
+// app.get('/music',()=>{
+//     console.log('music')
+// })
 
 app.listen(port,()=>{
     console.log('server is up and running')
